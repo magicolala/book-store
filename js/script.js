44e7d9381ac7ec01213db2005c62b08bf581fa61
Cédric Oloa <cedric.oloa@laposte.net>
2019-01-24 21:06:00 +0100

    add famous writers section

--- js/script.js
+++ js/script.js
@@ -12,4 +12,11 @@ $(document).ready(function() {
             $(".gallery").removeClass("change");
         }
     });
+
+    $(".writers-accordion").click(function(event) {
+        if (event.target.id.split("-")[0] === "button") {
+            $("#book-1").attr("src", "img/writers/" + event.target.id.split("-")[1] + "-book1.jpg");
+            $("#book-2").attr("src", "img/writers/" + event.target.id.split("-")[1] + "-book2.jpg");
+        }
+    });
 });
