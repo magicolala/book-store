a5530fa800e8547e6ccda3e0fa454e9c5715d876
Cédric Oloa <cedric.oloa@laposte.net>
2019-01-24 16:08:27 +0100

    add gallery section

--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
+$(document).ready(function() {
+    lightbox.option({
+        wrrapAround: true
+    });
 
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (position >= 350) {
+            $(".gallery").addClass("change");
+        } else {
+            $(".gallery").removeClass("change");
+        }
+    });
+});
