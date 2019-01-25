b7de68cd1176a93a254c93827b8ec8f53dd0d463
Cédric Oloa <cedric.oloa@laposte.net>
2019-01-25 23:35:18 +0100

    add navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    console.log("test");
+    $(".hamburger-menu").click(function() {
+        console.log("click");
+        $(".navigation").toggleClass("change");
+    });
+
     lightbox.option({
         wrrapAround: true
     });
